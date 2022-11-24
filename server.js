8c1e4069d31a3f9e70a08bd8b837c1689ef6e315
jwui <snoopy2080@gmail.com>
2022-11-24 15:31:55 +0900

    make it better

--- server.js
+++ server.js
@@ -38,6 +38,10 @@ app.get("/", function (req, res) {
   res.render("index", { userData: req.user }); //로그인시 회원정보데이터 ejs 파일로 전달
 });
 
+app.get("/login", (req, res) => {
+  res.render("admin_login"); // admin_login.ejs 파일로 응답
+});
+
 //  /loginresult 경로 요청시 passport.autenticate() 함수구간이 아이디 비번 로그인 검증구간
 passport.use(
   new LocalStrategy(
@@ -48,12 +52,10 @@ passport.use(
       passReqToCallback: false,
     },
     function (id, pass, done) {
-      // console.log(userid, userpass);
       db.collection("port3_user").findOne(
         { userId: id },
         function (err, result) {
           if (err) return done(err);
-
           if (!result)
             return done(null, false, {
               message: "존재하지않는 아이디 입니다.",
@@ -84,6 +86,22 @@ passport.deserializeUser(function (userId, done) {
       done(null, result);
     }
   );
+});
+
+//관리자 화면 로그인 유무 확인
+app.post(
+  "/admin",
+  passport.authenticate("local", { failureRedirect: "/fail" }),
+  (req, res) => {
+    res.redirect("/admin/store");
+    console.log(req.user);
+    //로그인 성공시 관리자 매장등록 페이지로 이동
+  }
+);
+
+//로그인 실패시 fail경로
+app.get("/fail", (req, res) => {
+  res.send("로그인 실패");
 });
 
 //파일첨부
@@ -156,6 +174,7 @@ app.get("/storeupt/:no", function (req, res) {
   //input, textarea에다가 작성내용 미리 보여줌
 });
 
+//매장 수정사항 db에 적용하는 구간
 app.post("/updatestore", function (req, res) {
   db.collection("port3_store").updateOne(
     { num: Number(req.body.num) },
@@ -177,11 +196,61 @@ app.post("/updatestore", function (req, res) {
   );
 });
 
-//상점찾기 페이지(사용자)
-app.get("/store", (req, res) => {
+//관리자 매장 삭제처리 get 요청
+app.get("/deletestore/:no", function (req, res) {
+  //db안에 해당 매장 번호에 맞는 데이터만 삭제 처리
+  db.collection("port3_store").deleteOne(
+    { num: Number(req.params.no) },
+    function (err, result) {
+      //매장 목록페이지로 이동
+      res.redirect("/admin/store");
+    }
+  );
+});
+
+app.get("/store", async (req, res) => {
+  //사용자가 게시판에 접속시 몇번 페이징 번호로 접속했는지 확인
+  let pageNum = req.query.page == null ? 1 : Number(req.query.page);
+  //한 페이지당 보여줄 데이터 갯수
+  let perPage = 5;
+  //블록당 보여줄 페이징 번호 갯수
+  let blockCount = 3;
+  //현재 페이지 블록 구하는 구간
+  let blockNum = Math.ceil(pageNum / blockCount);
+  //블록안에 있는 페이징의 시작번호 값
+  let blockStart = (blockNum - 1) * blockCount + 1;
+  //블록안에 있는 페이징의 끝번호 값
+  let blockEnd = blockStart + blockCount - 1;
+  //데이터베이스 콜렉션에 있는 전체 객체의 갯수값 가져오는 명령어
+  let totalData = await db.collection("port3_store").countDocuments({});
+  //전체 데이터값을 통해서 전체 페이징 번호를 계산
+  let paging = Math.ceil(totalData / perPage);
+  //블록에서 마지막 번호가 페이징의 끝번호보다 크다면, 페이징의 끝번호를 강제로 부여
+  if (blockEnd > paging) {
+    blockEnd = paging;
+  }
+
+  //블록의 총 갯수 계산
+  let totalBlock = Math.ceil(paging / blockCount);
+  //데이터베이스에서 꺼내오는 데이터의 순번값을 결정
+  //데이터베이스 콜렉션에서 데이터를 perPage 갯수만큼 맞춰서 가져오기
+  let startFrom = (pageNum - 1) * perPage;
+  //sort({정렬할 프로퍼티명:1}) 오름차순 -1은 내림차순
+  //조건문을 이용해서 입력한 검색어가 있는 경우는 aggregate({}).sort().skip().limit()
   db.collection("port3_store")
     .find({})
+    .sort({ number: -1 })
+    .skip(startFrom)
+    .limit(perPage)
     .toArray((err, result) => {
-      res.render("store", { storeData: result });
+      res.render("store", {
+        storeData: result,
+        paging: paging,
+        pageNum: pageNum,
+        blockStart: blockStart,
+        blockEnd: blockEnd,
+        blockNum: blockNum,
+        totalBlock: totalBlock,
+      });
     });
 });
